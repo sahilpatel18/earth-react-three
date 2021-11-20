a95d9d89f28f4fc30538a4d31c2e782c5102baac
sahilpatel18 <sahil.41801@gmail.com>
2021-11-19 20:22:41 -0500

    increased rotation speed

---
+++ src/components/earth/index.jsx
@@ -0,0 +1,72 @@
+import { useLoader, useFrame } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+
+import EarthDayMap from "../../textures/8k_earth_daymap.jpeg";
+import EarthNormalMap from "../../textures/8k_earth_normal_map.jpeg";
+import EarthSpecularMap from "../../textures/8k_earth_specular_map.jpeg";
+import EarthCloudMap from "../../textures/8k_earth_clouds.jpg";
+import { useRef } from "react";
+
+const Earth = () => {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudMap]
+  );
+
+  const earthRef = useRef();
+  const cloudsRef = useRef()
+
+  useFrame(({ clock }) => {
+      const elapsedTime = clock.getElapsedTime()
+      earthRef.current.rotation.y =elapsedTime / 5;
+      cloudsRef.current.rotation.y =elapsedTime / 5;
+  });
+
+  return (
+    <>
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={10000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
+      <mesh ref={cloudsRef}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+      <mesh ref={earthRef}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
+
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
+      </mesh>
+    </>
+  );
+};
+
+export default Earth;
